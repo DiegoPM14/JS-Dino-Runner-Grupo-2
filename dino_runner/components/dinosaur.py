5cc03fc9fb5c9c8fec8275f875074383fe94b60f
DiegoPM14 <diegopadilla174@gmail.com>
2022-10-10 20:54:23 -0500

    Tarea clase 1
    despues de ver en clase como implementar el correr y el saltar, se nos pidio realizar el agacharse

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,7 +2,7 @@ import pygame
 
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 
 class Dinosaur(Sprite):
@@ -16,21 +16,31 @@ class Dinosaur(Sprite):
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
         self.step_index = 0
-
         self.jump_velocity = self.JUMP_VEL
-
         self.dino_running= True
         self.dino_jumping= False
+        self.dino_ducking= False
         
     def update(self,user_input):
         if self.dino_running:
             self.run()
         elif self.dino_jumping:
             self.jump()
+        elif self.dino_ducking:
+            self.duck()
 
         if user_input[pygame.K_UP] or user_input[pygame.K_SPACE] and not self.dino_jumping:
             self.dino_jumping = True
-            self.dino_running= False
+            self.dino_running = False
+            self.dino_ducking = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jumping:
+            self.dino_jumping = False
+            self.dino_running = False
+            self.dino_ducking = True
+        elif not self.dino_jumping:
+            self.dino_jumping = False
+            self.dino_running = True
+            self.dino_ducking = False
         
         if self.step_index >= 10:
             self.step_index=0
@@ -51,6 +61,12 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POS
             self.dino_jumping = False
             self.jump_velocity = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index <5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y= self.Y_POS + 40
 
     def draw(self, screen):
         screen.blit(self.image,(self.dino_rect.x, self.dino_rect.y))
